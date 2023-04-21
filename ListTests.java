6d192b103b7b18a161a6920b103240eb3f4d439a
JpDs17 <130107012+JpDs17@users.noreply.github.com>
2023-04-20 17:45:13 -0700

    Final Progress 4/20

--- ListTests.java
+++ ListTests.java
@@ -17,5 +17,14 @@ public class ListTests {
         ListExamples.filter(input, sc);
         assertEquals(result, ListExamples.filter(input, sc));
     }
-    
+    @Test
+    public void testMerge(){
+        String[] input1 = {"a", "b", "c", "d"};
+        List<String> input = Arrays.asList(input1);
+        String[] input2 = {"a", "a", "b", "b", "c", "c", "d", "d"};
+        List<String> input3 = Arrays.asList(input2);
+        List<String> result = input;
+        assertEquals(input3, ListExamples.merge(input, result));
+    }
+       
 }
